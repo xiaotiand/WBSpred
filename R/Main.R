044d0521205e07a7ccc6172dcb9b2cb877460b13
xdai12 <xiaotian.dai.utah@gmail.com>
2023-11-04 22:40:51 -0500

    Enable Negative Binomial distribution

--- R/Main.R
+++ R/Main.R
@@ -1,59 +1,131 @@
 ##### Main Functions
 
-#' Create an .stan model
+#' Create a Poisson .stan model
 #'
 #' This is an internal function used to create a Stan model.
 #' @param P The highest power of WBS signal in regression
 #' @return The .stan model
 #' @examples
-#' cat(createStanModel(3))
+#' cat(createStanPoisson(2))
 
-createStanModel = function(P = 1) {
+createStanPoisson = function(P = 2) {
   scode = "data {
-  int<lower=0> P;
-  int<lower=0> N;
-  int<lower=0> L;
-  vector[L] evalues;
-  vector[N] WWmean;
-  matrix[N, L] WW;
-  vector[N] offset;
-  int<lower=0> y[N];
+    int<lower=0> N;
+    int<lower=0> L;
+    int<lower=0> Q;
+    vector[L] evalues;
+    vector[N] WWmean;
+    matrix[N, L] WW;
+    matrix[N, Q] covariate;
+    vector[N] offset;
+    int<lower=0> y[N];
   }
+
   parameters {
-  real beta0;
-  vector[L] betaL;
-  vector[P] betaP;
-  vector[N] phi;
-  real <lower=0> sigma_beta;
-  real <lower=0> sigma_betaL;
-  real alpha1;
-  real <lower=0> sigma_phi;
+    real beta0;
+    vector[L] betaL;
+    vector[2] betaP;
+    vector[Q] betaQ;
+    vector[N] phi;
+    real <lower=0> sigma_beta;
+    real <lower=0> sigma_betaL;
+    real alpha1;
+    real <lower=0> sigma_phi;
   }
+
   transformed parameters  {
-  vector[N] mu;
-  vector[N] wbe;
-  wbe = WWmean + WW * betaL;
-  mu = offset + beta0 + phi;
-  for (p in 1:P) {
-    mu = mu + betaP[p] * wbe .^ p;
+    vector[N] mu;
+    vector[N] wbe;
+    wbe = WWmean + WW * betaL;
+    mu = offset + beta0 + phi + sqrt(covariate) * betaQ;
+    mu = mu + betaP[1] * wbe + betaP[2] * wbe .* wbe;
   }
-  }
+
   model {
-  beta0 ~ normal(0, 10000);
-  for (l in 1:L) {
-    betaL[l] ~ normal(0, evalues[l]);
+    beta0 ~ normal(0, 10000);
+    for (l in 1:L) {
+      betaL[l] ~ normal(0, evalues[l]);
+    }
+    for (p in 1:2) {
+      betaP[p] ~ normal(0, sigma_beta);
+    }
+    for (q in 1:Q) {
+      betaQ[q] ~ normal(0, sigma_beta);
+    }
+    sigma_beta ~ inv_gamma(0.1, 0.1);
+    phi[1] ~ normal(0, sigma_phi);
+    for (i in 2:N) {
+      phi[i] ~ normal(alpha1*phi[i-1], sigma_phi);
+    }
+    alpha1 ~ uniform(-1, 1);
+    sigma_phi ~ inv_gamma(0.1, 0.1);
+    y ~ poisson(exp(mu));
+  }"
+
+  return(scode)
+}
+
+
+#' Create a Negative Binomial .stan model
+#'
+#' This is an internal function used to create a Stan model.
+#' @param P The highest power of WBS signal in regression
+#' @return The .stan model
+#' @examples
+#' cat(createStanNB(2))
+
+createStanNB = function(P = 2) {
+  scode = "data {
+    int<lower=0> N;
+    int<lower=0> L;
+    int<lower=0> Q;
+    vector[L] evalues;
+    vector[N] WWmean;
+    matrix[N, L] WW;
+    matrix[N, Q] covariate;
+    vector[N] offset;
+    int<lower=0> y[N];
   }
-  for (p in 1:P) {
-    betaP[p] ~ normal(0, sigma_beta);
+
+  parameters {
+    real beta0;
+    vector[L] betaL;
+    vector[2] betaP;
+    vector[Q] betaQ;
+    vector[N] phi;
+    real <lower=0> sigma_beta;
+    real <lower=0> sigma_betaL;
+    real alpha1;
+    real <lower=0> sigma_phi;
   }
-  sigma_beta ~ inv_gamma(0.1, 0.1);
-  phi[1] ~ normal(0, sigma_phi);
-  for (i in 2:N) {
-    phi[i] ~ normal(alpha1*phi[i-1], sigma_phi);
+
+  transformed parameters  {
+    vector[N] mu;
+    vector[N] wbe;
+    wbe = WWmean + WW * betaL;
+    mu = offset + beta0 + phi + sqrt(covariate) * betaQ;
+    mu = mu + betaP[1] * wbe + betaP[2] * wbe .* wbe;
   }
-  alpha1 ~ uniform(-1, 1);
-  sigma_phi ~ inv_gamma(0.1, 0.1);
-  y ~ poisson(exp(mu));
+
+  model {
+    beta0 ~ normal(0, 10000);
+    for (l in 1:L) {
+      betaL[l] ~ normal(0, evalues[l]);
+    }
+    for (p in 1:2) {
+      betaP[p] ~ normal(0, sigma_beta);
+    }
+    for (q in 1:Q) {
+      betaQ[q] ~ normal(0, sigma_beta);
+    }
+    sigma_beta ~ inv_gamma(0.1, 0.1);
+    phi[1] ~ normal(0, sigma_phi);
+    for (i in 2:N) {
+      phi[i] ~ normal(alpha1*phi[i-1], sigma_phi);
+    }
+    alpha1 ~ uniform(-1, 1);
+    sigma_phi ~ inv_gamma(0.1, 0.1);
+    y ~ neg_binomial_2(exp(mu), 100);
   }"
 
   return(scode)
@@ -77,6 +149,7 @@ createStanModel = function(P = 1) {
 #' @param end_date End date of training data used to build model
 #' @param StanModel Created Stan model
 #' @param iteration A positive integer specifying the number of iterations for each chain (including burnin).
+#' @param distribution Distribution of outcome variable: "Poisson" or "NB".
 #' @return fit The fitted MCMC object
 #' @examples
 #' modeldata = as.data.table(readRDS("modeldata.rds"))
@@ -85,7 +158,7 @@ createStanModel = function(P = 1) {
 #'                       start_date, end_date, iteration)
 
 PoissonReg = function(modeldata, date, p.cases, p.rate, ww.signal, lag = 1,
-                      start_date, end_date, StanModel, iteration) {
+                      start_date, end_date, StanModel, iteration, distribution = "Poisson") {
   ldata = as.data.frame(modeldata[, names(modeldata) %in% c(date, ww.signal), with = FALSE])
   cdata = as.data.frame(modeldata[, names(modeldata) %in% c(date, p.cases, p.rate), with = FALSE])
   cdata$offset = round(cdata$clinical.cases / cdata$positivity.rate * 100)
@@ -116,7 +189,13 @@ PoissonReg = function(modeldata, date, p.cases, p.rate, ww.signal, lag = 1,
               WW = as.data.frame(finaldata[, grep("ww_signal", names(finaldata))]),
               offset = log(finaldata$offset),
               y = finaldata[, which(names(finaldata) %in% p.cases)])
-  regression_model = stan_model(createStanModel(P = data$P))
+  if (distribution = "Poisson") {
+    regression_model = stan_model(createStanPoisson(P = data$P))
+  } else if (distribution = "NB") {
+    regression_model = stan_model(createStanNB(P = data$P))
+  } else {
+    stop("Outcome distribution not supported")
+  }
   fit = rstan::sampling(regression_model, data = data, chains = 2, iter = iteration, refresh = 0)
 
   return(list(lag = lag,
@@ -187,3 +266,4 @@ predictPoisson = function(modelres, modeldata, pred_start, pred_end, iteration) 
   return(list(actual = finaldata,
               prediction = predmat))
 }
+
